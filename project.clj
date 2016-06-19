9f55fd265902958ea31adbc73769d169b64fb96f
ESC <youhavethewrong@gmail.com>
2016-06-19 09:37:12 -0500

    Initial

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject scratchpad "0.1.0-SNAPSHOT"
+  :description "A place to mess around with ideas"
+  :url "https://github.com/youhavethewrong/scratchpad"
+  :license {:name "Apache License, Version 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
